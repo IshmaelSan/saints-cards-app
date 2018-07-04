b173d9572b835aed1875dd1c81a553766ce7d8cf
unknown <enginass@yahoo.com>
2018-07-04 16:19:06 -0400

    Fixed errors w/ question & answer binding

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -9,34 +9,47 @@ import { Card } from '../models/Card';
 })
 export class CardsComponent implements OnInit {
 
-	cards:Card[];
-  index:number=0;
+	cards:Card[];//array of cards/a dick
+  index=1;//iniate index at 1 since onInit has 0th index
+  currentQuestion:string;//this.currentQuestion from cards/deck
+  currentAnswer:string;//this.currentAnswer from cards/deck
 
-
-  constructor(private cardService: CardsService) {
+  constructor(private cardService: CardsService) {//pass in cardService from services folder
 
   }
 
   ngOnInit() {
   	this.cardService.getCards().subscribe(cards =>{
   		console.log("length: "+cards.length);
-  		this.cards = cards;
-  	});
+  		this.cards = cards;//make a place in array for each card in deck and place in cards[]
+      if(cards.length>0){
+        this.currentQuestion=this.cards[0].question;//populate currentQuestion binding with 0th question in cards array
+        this.currentAnswer=this.cards[0].answer;//populate currentAnswer
+      }
+    });
   }
-
   
-  getQuestion(): string{
-    return this.cards[this.index].question;
+  getQuestion(){
+    this.currentQuestion=this.cards[this.index].question;//update question
+    console.log(this.currentQuestion);
   }
 
-  getAnswer(): string{
-    return this.cards[this.index].answer;
+  getAnswer(){
+    this.currentAnswer=this.cards[this.index].answer;//update answer
+    console.log(this.currentAnswer);
   }
 
   nextCard(){
-    if(this.index<this.cards.length)
-    this.index+=1;
-    console.log("button clicked");
+    if(this.index<this.cards.length){//if more cards exist, get next question & answer
+      console.log("button clicked.  index: "+this.index);
+      this.getQuestion();
+      this.getAnswer();
+      this.index++;
+    }
+    else{
+      this.currentQuestion='--end of deck--';
+      console.log('keine Karten mehr. index: '+this.index);//no more cards
+    }
   }
 
 }
--- src/app/services/cards.service.ts
+++ src/app/services/cards.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Observable } from 'rxjs';
 import { Card } from '../models/card';
+import { map } from 'rxjs/operators';//why is this not included in rxjs?  just to make things difficult?
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,19 @@ export class CardsService {
 	cards: Observable<Card[]>;
 
   constructor(public afs: AngularFirestore) {
-  this.cards= this.afs.collection('deck').valueChanges();
+  //this.cards= this.afs.collection('deck').valueChanges();
+  /*this.cards = this.afs.collection('deck').snapshotChanges().map(changes => {
+  	return changes.map(a => {
+  		const data = a.payload.doc.data() as Card;
+  		data.id = a.payload.doc.id;
+  		return data;
+  	});**********took me forever to figure out that Angular 5 uses a pipe to do this*****
+  });*/
+  this.cards = this.afs.collection('deck').snapshotChanges().pipe(map(changes => changes.map(a => {
+  	const data = a.payload.doc.data() as Card;
+  	data.id = a.payload.doc.id;
+  	return data;//map observable firebase db collection into Card array
+  })))
    }
 
    getCards(){
